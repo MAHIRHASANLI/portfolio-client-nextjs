b9388c37cc586069c9237ac4b2f33e190f0d0acd
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2024-11-05 01:12:21 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
 import React from "react";
 import UserContainer from "@/container/user";
+import type { Metadata } from "next";
+
+  title: "Mahir :: Web Developer",
+  description:
+    "I am a software engineer, developing web applications. Feel free to get in touch with me by mailing hasanlimahir1@gmail.com",
+  keywords:
+    "html, javascript,typescript, react, react js, next, next js, node,node js, tailwind, css,developer, Mern stack, Frontend,  Portfolio, Mahir Hasanli,Mahir Hasani, Mahir Həsənli, Mahir, Hasanli, mahir, hasanli, mahirhasanli, mahirhasani, Mahir.dev, mahirhasani.vercel.app",
+  authors: [{ name: "Mahir Hasanli" }],
+  robots: "index, follow",
+  openGraph: {
+    title: "Mahir :: Web Developer",
+    description:
+      "I am a software engineer, developing web applications. Feel free to get in touch with me by mailing hasanlimahir1@gmail.com",
+    url: "https://mahirhasani.vercel.app",
+    type: "website",
+  },
+  twitter: {
+    title: "Mahir :: Web Developer",
+    description:
+      "I am a software engineer, developing web applications. Feel free to get in touch with me by mailing hasanlimahir1@gmail.com",
+    card: "summary_large_image",
+  },
+  verification: {
+    google:
+      "google-site-verification=dFXv5OH_VewFRYTwG5pq0bC3AFyGCDZcvqRJCFpcBW4",
+  },
+};
+
 // type PropsPage = {
 //   searchParams: { [key: string]: string | boolean };
 // };
