9927b623256f8e7527a1952420956bc1b82b20b1
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2024-11-05 01:11:02 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,8 @@
-import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "../styles/reset.css";
 import "../styles/global.css";
 
-export const metadata: Metadata = {
-  title: "Mahir :: Web Developer",
-  description:
-    "I am a software engineer, developing web applications. Feel free to get in touch with me by mailing hasanlimahir1@gmail.com",
-  keywords:
-    "html, javascript,typescript, react, react js, next, next js, node,node js, tailwind, css,developer, Mern stack, Frontend,  Portfolio, Mahir Hasanli,Mahir Hasani, Mahir Həsənli, Mahir, Hasanli, mahir, hasanli, mahirhasanli, mahirhasani, Mahir.dev, mahirhasani.vercel.app",
-  authors: [{ name: "Mahir Hasanli" }],
-  robots: "index, follow",
-  openGraph: {
-    title: "Mahir :: Web Developer",
-    description:
-      "I am a software engineer, developing web applications. Feel free to get in touch with me by mailing hasanlimahir1@gmail.com",
-    url: "https://mahirhasani.vercel.app",
-    type: "website",
-  },
-  twitter: {
-    title: "Mahir :: Web Developer",
-    description:
-      "I am a software engineer, developing web applications. Feel free to get in touch with me by mailing hasanlimahir1@gmail.com",
-    card: "summary_large_image",
-  },
-  verification: {
-    google:
-      "google-site-verification=dFXv5OH_VewFRYTwG5pq0bC3AFyGCDZcvqRJCFpcBW4",
-  },
-};
+
 
 const poppins = Montserrat({
   weight: ["400", "500", "700"],
