4f7cd816747c423b5df83f0aaa90f10a89b9164f
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2024-11-25 23:49:46 +0400

    Update index.tsx

--- components/user_header/user_badge/index.tsx
+++ components/user_header/user_badge/index.tsx
@@ -7,17 +7,17 @@ import { getDataAbout } from "@/api/get_requests";
 const USerBadge: React.FC = async () => {
   const [{ image }] = await getDataAbout();
   return (
-    <Link href="https://mahirhasanli.vercel.app/">
+    <Link href="https://mahirhasani.vercel.app/">
       <div className={styles["hero-fullName"]} key="">
         <Image
           className={styles.img}
           src={image}
-          alt="Mahir Hasani"
+          alt="Mahir Hasanli"
           width={50}
           height={50}
         />
         <span>Mahir</span>
-        <span style={{ color: "#7843E9" }}>Hasani</span>
+        <span style={{ color: "#7843E9" }}>Hasanli</span>
       </div>
     </Link>
   );
